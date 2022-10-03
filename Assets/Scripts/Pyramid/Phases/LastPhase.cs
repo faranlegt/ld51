b47e6a9f6536e9d0cbdcd8d427ce64e6b7621bc9
Yegor Zhumikov <yegor@yogurtthehor.se>
2022-10-03 23:18:35 +0600

    feat: spider spawn

--- Assets/Scripts/Pyramid/Phases/LastPhase.cs
+++ Assets/Scripts/Pyramid/Phases/LastPhase.cs
@@ -1,3 +1,6 @@
+using System;
+using DetachedBlocks;
+using DG.Tweening;
 using Effects;
 using Emitters;
 using MyBox;
@@ -7,10 +10,21 @@ namespace Pyramid.Phases
 {
     public class LastPhase : MonoBehaviour, IPhase
     {
-        public WaitingBlock[] enabledDrops = {};
-        
+        public WaitingBlock[] enabledDrops = { };
+
         public int spotsToRemove = 2;
         public Barrel barrel;
+
+        public LineAnimator baseAnimator;
+
+        [Header("Spider spawn")] 
+        public Spider spider;
+
+        public Transform spawnPlace;
+
+        public float spawnTime = 7.5f;
+        
+        private Sequence _spawning;
 
         private void Start()
         {
@@ -18,8 +32,24 @@ namespace Pyramid.Phases
             {
                 Singleton<WaitingBlockDropper>.Instance.AddBlockForDrop(waitingBlock);
             }
+
+            baseAnimator.onNewFrame.AddListener(CheckFrame);
+
+            _spawning = DOTween
+                .Sequence()
+                .PrependInterval(spawnTime)
+                .OnStepComplete(() => baseAnimator.LaunchOnce(baseAnimator.sprites))
+                .SetLoops(-1);
         }
-        
+
+        private void CheckFrame(int frame)
+        {
+            if (frame != 8)
+                return;
+
+            Instantiate(spider, spawnPlace.position, Quaternion.identity);
+        }
+
         public void WeakSpotRemoved()
         {
             spotsToRemove--;
@@ -32,6 +62,7 @@ namespace Pyramid.Phases
 
         private void FinishGame()
         {
+            _spawning.Complete();
             Singleton<EffectsSpawner>.Instance.Explode(transform.position, 3);
             barrel.Destroy();
         }
